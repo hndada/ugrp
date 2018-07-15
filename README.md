65230dd51f4e23b9fab116f0ad3f8ed0d52839ca
hndada <hndada@dgist.ac.kr>
2018-07-16 05:15:52 +0900

    implement vector_calculator and README

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
-# ugrp
+##sdvx
+#손교차곡 list (add more please)
+Lv18 Juggler's Madness
 
-#To Do @sdvx
-손처리 좌우 판별 
+Lv18 #Endroll (needs lots of trial and error)
+(no.65~72) (약간 어색할 것으로 예상.)
+(no.73~76)
+(no.77~80: 손교차가 아니나 현 알고리즘으로는 복잡한 손배치가 구현됨)
+
+Lv20 I (no.65~71) 
+:difficult to determine L/R automatically. will just try manually
+
+Lv19 Staring at Star (no.34) 
+Success! by Pure Luck
+
+#To Do
+손처리 좌우 판별 (done)
 벡터 도입
 
+#idea bank
+짧은 벡터가 많을수록 밀집해있다
+벡터 길이가 일정 이상 되면, 무효화. '손을 떼고 있어도 무방'
+
+
+
+##popn
 
 
 #참고 자료
@@ -12,5 +32,3 @@ http://dancedancerevolutionddr.wikia.com/wiki/Stream_(Groove_Radar)#How_to_Calcu
 
 osu!mania Difficulty Calculator
 https://github.com/ppy/osu/blob/master/osu.Game.Rulesets.Mania/Difficulty/ManiaDifficultyCalculator.cs
-
-#idea bank
--- sdvx/vector_calculator.py
+++ sdvx/vector_calculator.py
@@ -7,7 +7,7 @@ and totally same at the other side but 'R'
 3-1. a.k.a. particular hand position: "hand-crossing(손교차)'
 ->only when one of knob is "stay"
 3-2.'more easy hand position if one use his or her "technical skill"
-(기교를 써서 더 쉬운 손배치가 가능한 경우 -> HEAVENLY SMILE MXM)'
+(기교를 써서 더 쉬운 손배치가 가능한 경우 -> HEAVENLY SMILE MXM)' 
 """
 
 #duplicated code start
@@ -47,9 +47,9 @@ for i in range(len(c_cont_all)):
 #print("tune:", tune)
 
 
-print(c_cont[2])
-print(c_cont[2][0])
-print(c_cont[2][0][0])
+#print(c_cont[2])
+#print(c_cont[2][0])
+#print(c_cont[2][0][0])
 #format: c_cont[#no. tune] #len(c_cont[tune])= 1+1 or 4 or 8+1, ...
 notelist=[]
 infolist=[]
@@ -67,3 +67,43 @@ for i in range(tune):
 #print(infolist)
 
 #duplicated code end
+
+count_cross=0
+for i in range(tune): #i: number of tunes
+    for j in range(notelist[i]+len(infolist[i])): #j: number of lines in a tune
+        if(j not in infolist[i]):
+            #c_cont[i][j]: 1111|00|--
+            listline=list(c_cont[i][j])
+            hand=0
+            cross=0 #indicate whether hand-crossing is occurred
+            while(hand!=10):
+                if(hand in [0, 1, 5]): #Left side
+                    if (listline[hand]!='0'): #left side is activated
+                        if (listline[8] not in ['-',':']): #knob-L is also activated
+                            if(listline[9] in ['-',':']): #when knob-R's state is 'stay'
+                                cross=1 #run 결과 손교차 횟수가 거의 손 이동 횟수 수준으로 나옴 
+                                listline[hand]='L' #then we can hit normally
+                            else:
+                                listline[hand]='R'
+                        else:
+                            listline[hand]='L'
+                if(hand in [2, 3, 6]): #right side
+                    if (listline[hand]!='0'): #right side is activated
+                        if (listline[9] not in ['-',':']): #knob-R is also activated
+                            if(listline[8] in ['-',':']): #when knob-L's state is 'stay'
+                                cross=1
+                                listline[hand]='R' #then we can hit normally
+                            else:
+                                listline[hand]='L'
+                        else:
+                            listline[hand]='R'
+                if(hand==8): #knob-L
+                    if (listline[hand]!='-'):
+                        listline[hand]='R' if cross else 'L'
+                if(hand==9): #knob-R
+                    if (listline[hand]!='-'):
+                        listline[hand]='L' if cross else 'R'
+                hand+=1
+            count_cross+=cross
+
+print("count_cross:",count_cross)
