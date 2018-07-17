b21df4d1e0d077b958f0dd26fb8aedc15ea82df4
hndada <hndada@dgist.ac.kr>
2018-07-18 08:01:54 +0900

    improved determining the side

--- sdvx/vector_calculator.py
+++ sdvx/vector_calculator.py
@@ -66,44 +66,122 @@ for i in range(tune):
     notelist.append(noteline)
 #print(infolist)
 
-#duplicated code end
+#duplicated code end 
+#3.(심화): 주차가 2개의 소마디 이상으로 진행되면 '조작없음'으로 판단. or 그냥 주차는 조작없음으로.
 
-count_cross=0
+#현재 라인에 노브가 있는지 확인.
+#칩노트: 결과적으로 노브가 무조건 우선시.
+#노브없으면 기본값으로 배정 (현재 기교 무시)
+#노브있으면, 노브의 반댓손으로 처리.
+
+#홀드노트: 기존에 있는 걸 우선시함.
+#동시에 나올 경우 노브 > 홀드 > 칩. 노브와 홀드가 동시에 같은 손으로 처리되지 않는것을 원칙으로 한다. (counting)
+#노브있으나 홀드on이면 홀드 손 유지. 노브를 반대손으로 처리
+#노브있으나 홀드off이면 노브 손 기본값, 홀드를 반대손으로 처리
+#한번 홀드on하면 끝날때까지 손을 바꾸지 않음.
+
+holdon=0 #check if the hold is already on(active)
+holdside='N' #L/R: Left/Right   B:Both; also granted when other hand is free N:Nullity; something went wrong
+knobon=0 #check if the knob is already on(active)
+knobside='N'
+
+count_holdon=0
+def otherside(hand, side):
+    if side=='L':
+        return 'R'
+    if side=='R':
+        return 'L'
+    if side=='B':
+        if hand in [0, 1, 5, 8]:
+            return 'R'
+        if hand in [2, 3, 6, 9]:
+            return 'L' 
+    if side=='N':
+        return 'N'
+
+def side(hand):
+    if hand in [0,1,5,8]:
+        return 'L'
+    if hand in [2,3,6,9]:
+        return 'R'
+
+def check_code(mode, hand):
+    if (mode=='chip'):
+        if hand in [0,1,2,3]:
+            return '1'
+        if hand in [5,6]:
+            return '2'
+    if (mode=='hold'):
+        if hand in [0,1,2,3]:
+            return '2'
+        if hand in [5,6]:
+            return '1'
+
+f2=open("./ksh/directed_"+sys.argv[1].replace('./ksh/',''),"w",encoding="UTF8")
+f2.write('\n'.join(c_info)+'\n')
+
 for i in range(tune): #i: number of tunes
+    f2.write('--\n')
     for j in range(notelist[i]+len(infolist[i])): #j: number of lines in a tune
-        if(j not in infolist[i]):
-            #c_cont[i][j]: 1111|00|--
+        if(j not in infolist[i]): #c_cont[i][j]: 1111|00|--
             listline=list(c_cont[i][j])
-            hand=0
-            cross=0 #indicate whether hand-crossing is occurred
-            while(hand!=10):
-                if(hand in [0, 1, 5]): #Left side
-                    if (listline[hand]!='0'): #left side is activated
-                        if (listline[8] not in ['-',':']): #knob-L is also activated
-                            if(listline[9] in ['-',':']): #when knob-R's state is 'stay'
-                                cross=1 #run 결과 손교차 횟수가 거의 손 이동 횟수 수준으로 나옴 
-                                listline[hand]='L' #then we can hit normally
-                            else:
-                                listline[hand]='R'
+            hand=8
+            while(hand!=7):
+                if(holdon):
+                    check_hold=[]
+                    for x in [0,1,2,3,5,6]:
+                        check_hold.append(listline[x]!=check_code('hold',x))
+                    if(all(check_hold)):
+                        holdon=0
+                        count_holdon+=1
+                if(knobon):
+                    check_knob=[]
+                    for x in [8,9]:
+                        check_knob.append(listline[x] in ['-',':'])
+                    if(all(check_knob)):
+                        knobon=0
+                """ #dunno which part is syntax error
+                if(holdon):
+                    holdon=0 if(all([listline[x]!=check_code('hold',x) for x in [0,1,2,3,5,6]]))
+                if(knobon):
+                    knobon=0 if(all(listline[x] in ['-',':'] for x in [8,9]))
+                """
+                if(hand in [8,9]):
+                    if (listline[hand] not in ['-',':']):
+                        if(holdon):
+                            listline[hand]=otherside(hand, holdside)
                         else:
-                            listline[hand]='L'
-                if(hand in [2, 3, 6]): #right side
-                    if (listline[hand]!='0'): #right side is activated
-                        if (listline[9] not in ['-',':']): #knob-R is also activated
-                            if(listline[8] in ['-',':']): #when knob-L's state is 'stay'
-                                cross=1
-                                listline[hand]='R' #then we can hit normally
+                            listline[hand]=side(hand)
+                        knobon=1
+                        knobside=listline[hand]
+                    else:
+                        listline[hand]=' '
+                if(hand in [0,1,2,3,5,6]):
+                    if (listline[hand]!='0'):
+                        if(listline[hand]==check_code('chip', hand)):
+                            if(knobon):
+                                listline[hand]=otherside(hand, knobside)
                             else:
-                                listline[hand]='L'
-                        else:
-                            listline[hand]='R'
-                if(hand==8): #knob-L
-                    if (listline[hand]!='-'):
-                        listline[hand]='R' if cross else 'L'
-                if(hand==9): #knob-R
-                    if (listline[hand]!='-'):
-                        listline[hand]='L' if cross else 'R'
+                                listline[hand]=side(hand)
+                        elif(listline[hand]==check_code('hold', hand)):
+                            if(knobon):
+                                listline[hand]=otherside(hand, knobside)
+                            else:
+                                if(listline[8] not in ['-',':']):
+                                    listline[hand]=otherside(8,side(8))
+                                elif(listline[9] not in ['-',':']):
+                                    listline[hand]=otherside(9,side(9))
+                                else:
+                                    listline[hand]=side(hand)
+                                holdon=1
+                                holdside=listline[hand]
+                    else:
+                        listline[hand]=' '
                 hand+=1
-            count_cross+=cross
-
-print("count_cross:",count_cross)
+                if hand==10:
+                    hand=0
+            f2.writelines(''.join(listline)+'\n')
+        else:
+            f2.write(c_cont[i][j]+'\n')
+f2.close()
+#print(count_holdon)
